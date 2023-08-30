6762fe042f2de885cabdd554287ab48a7f7f8fc8
Vitaliy Tarassov <apxustudio@gmail.com>
2023-08-30 19:06:39 +0600

    the rest tomorrow

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-const mySwiper = new Swiper('.swiper', {
-    loop: true,
-    autoplay: {
-    delay: 3000,
-    },
-    effect: 'fade',
-    fadeEffect: {
-    crossFade: true
-    },
-});
+// const mySwiper = new Swiper('.swiper', {
+//     loop: true,
+//     autoplay: {
+//     delay: 3000,
+//     },
+//     effect: 'fade',
+//     fadeEffect: {
+//     crossFade: true
+//     },
+// });
